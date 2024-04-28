3e1a296d92259d59103e684a8e6aba1de7f529c9
Manuel <manuelinfosec@gmail.com>
2024-04-28 01:43:15 +0100

    (imdb-scraper-rs): scraper grabbing top 100

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use reqwest;
+use scraper::{Html, Selector};
+
 fn main() {
-    println!("Hello, world!");
+    // IMDB Top 100 movies page
+    const IMDB_WEBSITE: &str =
+        "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100";
+
+    // send a request and extract page source
+    let page_source: String = reqwest::blocking::get(IMDB_WEBSITE)
+        .unwrap()
+        .text()
+        .unwrap();
+
+    // parse page source for querying
+    let document: Html = Html::parse_document(&page_source);
+
+    // selector h3 tags with the `ipc-title__text` class
+    let title_selector: Selector = Selector::parse("h3.ipc-title__text").unwrap();
+
+    // query document with selector and iterate first 100 element extracting string
+    let titles = document
+        .select(&title_selector)
+        .take(100) // 101th element is not a title (wrong selector)
+        .map(|item: scraper::ElementRef<'_>| -> String { item.inner_html() });
+
+    // print first 100 titles excluding last element (wrong)
+    titles.for_each(|item| println!("{item}"))
 }
